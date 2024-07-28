8ef75dd1607078aa2d531b076ab3491da79d4aab
Samarth <71966305+SamarthMotka@users.noreply.github.com>
2024-07-28 15:49:39 +0530

    done zoom img js

--- cerastar-website/static/script.js
+++ cerastar-website/static/script.js
@@ -6,57 +6,29 @@
 
 const cardData = [
     {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
+        imageUrl: "../media/products/download jpg/Glossy_800x1600MM-images-7.jpg",
         description: "PGVT 600 x 600",
         downloadUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/WL_PGVT-3_Staturio_600x600mm.pdf",
         viewUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/WL_PGVT-3_Staturio_600x600mm.pdf",
         category: "60" // Add your category name here
     },
     {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
+        imageUrl: "../media/products/download jpg/Glossy_800x1600MM-images-63.jpg",
         description: "WL 600 x 600",
         downloadUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/WL_3D+Series_600x600mm.pdf",
         viewUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/WL_3D+Series_600x600mm.pdf",
         category: "60" // Add your category name here
     },
     {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
+        imageUrl: "../media/products/download jpg/Glossy_800x1600MM-images-53.jpg",
         description: "Rainbow 600 x 1200",
         downloadUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/High+Gloss_800x1600+MM.pdf",
         viewUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/High+Gloss_800x1600+MM.pdf",
         category: "12" // Add your category name here
     },
     {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
-        description: "Glossy 600 x 1200",
-        downloadUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/Rubito+Glossy+Master+Collection+2022+V1.pdf",
-        viewUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/Rubito+Glossy+Master+Collection+2022+V1.pdf",
-        category: "12" // Add your category name here
-    },
-    {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
-        description: "HIGH GLOSS 800 x 1600",
-        downloadUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/Carving_800x1600+MM.pdf",
-        viewUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/Carving_800x1600+MM.pdf",
-        category: "16" // Add your category name here
-    },
-    {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
-        description: "Craving 800 x 1600",
-        downloadUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/Carving_800x1600+MM.pdf",
-        viewUrl: "https://cerastar.s3.ap-south-1.amazonaws.com/s3/Carving_800x1600+MM.pdf",
-        category: "16" // Add your category name here
-    },
-    {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        downloadUrl: "../media/products/pdf/Glossy_800x1600MM.pdf",
-        viewUrl: "../media/products/pdf/Glossy_800x1600MM.pdf",
-        category: "24" // Add your category name here
-    },
-    {
-        imageUrl: "https://res.cloudinary.com/da5crwlqh/image/upload/v1718292543/Khoj/Wearables/Kids/luaca1bx8ocn7ho6st9r.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        imageUrl: "../media/products/download jpg/Glossy_800x1600MM-images-65.jpg",
+        description: "12 x 24",
         downloadUrl: "../media/products/pdf/Glossy_800x1600MM.pdf",
         viewUrl: "../media/products/pdf/Glossy_800x1600MM.pdf",
         category: "24" // Add your category name here
@@ -64,7 +36,6 @@ const cardData = [
     // Add more card data objects as needed
 ];
 
-// Function to create a card element
 function createCard(cardData) {
     const card = document.createElement("div");
     card.classList.add("col-lg-3", "col-md-6", "portfolio-item", "isotope-item", "item", cardData.category);
@@ -77,6 +48,12 @@ function createCard(cardData) {
             <p class="mt-4 ">${cardData.description}</p>
         </div>
         <div class="display-btn">
+
+            <a class="card-link" href="${cardData.downloadUrl}" target="_blank" download>
+                          Sample CSV
+                        </a>
+
+
             <a href="${cardData.downloadUrl}" download >DOWNLOAD</a>
             <a href="${cardData.viewUrl}" target="_blank" class="view-pdf">VIEW</a>
         </div>
@@ -85,6 +62,9 @@ function createCard(cardData) {
 
     return card;
 }
+
+// document.body.appendChild(createCard(cardData));
+
 
 // Function to initialize the cards
 function initCards() {
@@ -158,12 +138,15 @@ function showPage() {
 
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.getElementById("navbarr");
+    var chg_size = document.getElementById("logo-sz");
     
     window.onscroll = function() {
         if (window.scrollY > 0) {
             navbar.classList.add("scrolled");
+            chg_size.classList.add("change-size");
         } else {
             navbar.classList.remove("scrolled");
+            chg_size.classList.remove("change-size");
         }
     };
 });
@@ -185,44 +168,62 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+// zoom product 
+
+const lens = document.querySelector('.magnify');
+const product_img = document.querySelector('.zoom');
+const magnified_img = document.querySelector('.magnified');
 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const slides = document.querySelectorAll('.swiper-slide img');
-    slides.forEach(slide => {
-        slide.addEventListener('mousemove', function(event) {
-            const lens = createLens(event, slide);
-            moveLens(event, lens, slide);
-            slide.parentElement.appendChild(lens);
+function magnify(product_image,magnify){
+    lens.addEventListener('mousemove',moveLens)
+    product_img.addEventListener('mousemove',moveLens)
+    lens.addEventListener('mouseout',leaveLens)
+}
 
-            slide.addEventListener('mousemove', (e) => moveLens(e, lens, slide));
-            slide.addEventListener('mouseout', () => lens.remove());
-        });
-    });
+function moveLens(e) {
 
-    function createLens(event, slide) {
-        const lens = document.createElement('div');
-        lens.classList.add('zoom-lens');
-        lens.style.backgroundImage = `url(${slide.src})`;
-        return lens;
-    }
+    const product_image_rect = product_img.getBoundingClientRect();
 
-    function moveLens(event, lens, slide) {
-        const slideRect = slide.getBoundingClientRect();
-        const lensSize = 100;
-        let x = event.clientX - slideRect.left - lensSize / 2;
-        let y = event.clientY - slideRect.top - lensSize / 2;
+    let x,y,cx,cy;
+    x = e.pageX - product_image_rect.left - lens.offsetWidth /2;
+    y = e.pageY - product_image_rect.top - lens.offsetHeight /2;
 
-        if (x > slide.width - lensSize) x = slide.width - lensSize;
-        if (x < 0) x = 0;
-        if (y > slide.height - lensSize) y = slide.height - lensSize;
-        if (y < 0) y = 0;
+    
+    
+    let max_xpos = product_image_rect.width -lens.offsetWidth;
+    let max_ypos = product_image_rect.height -lens.offsetHeight;
+    
+    if(x > max_xpos) x = max_xpos;
+    if(x < 0) x = 0;
+    
+    if(y > max_ypos) y = max_ypos;
+    if(y < 0) y = 0;
+    
+    lens.style.cssText = `top: ${y}px; left: ${x}px`;
 
-        lens.style.left = `${x}px`;
-        lens.style.top = `${y}px`;
-        lens.style.backgroundPosition = `-${x * 1.5}px -${y * 1.5}px`;
-        lens.style.display = 'block';
-        lens.style.width = `${lensSize}px`;
-        lens.style.height = `${lensSize}px`;
-    }
-});
+    cx = magnified_img.offsetWidth / lens.offsetWidth
+    cy = magnified_img.offsetHeight / lens.offsetHeight
+
+    magnified_img.style.cssText = `
+                background : url('${product_img.src}')
+                -${x * cx}px -${y*cy}px /
+                ${product_image_rect.width *cx}px ${product_image_rect.height * cy}px
+                no-repeat
+    `
+lens.classList.add('active');
+magnified_img.classList.add('active');
+
+}
+
+function leaveLens(){
+    lens.classList.remove('active');
+    magnified_img.classList.remove('active');
+}
+
+
+magnify(product_img,magnify);
+
+
+
+  
